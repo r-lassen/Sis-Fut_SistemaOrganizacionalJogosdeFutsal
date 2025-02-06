22eb5fb0fb0b21b58b38057377efd324eed899ab
Rafaela Ranieri Lassen <rafaela.44966@estagio.santanadeparnaiba.sp.gov.br>
2025-02-06 15:10:35 -0300

    Adicionar arquivos de projeto.

---
+++ SisºFut_SistemaOrganizacionalJogosdeFutsal/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SisºFut_SistemaOrganizacionalJogosdeFutsal.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
